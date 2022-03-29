b6144baec09f86e72a7dd2fcf5abc5eaed8071a1
Simon Frankenberger <simon@fraho.eu>
2022-03-29 14:14:32 +0200

    format code

--- pics/spieler.php
+++ pics/spieler.php
@@ -1,52 +1,52 @@
-<?php	
-	/**
-		* Liest ein Spielerbild aus und gibt es zurück
-		* 
-		* @version 1.0.0
-		* @author Simon Frankenberger <simonfrankenberger@web.de>
-		* @package blm2.includes
-	*/
-	
-	if(!$_GET['uid'])
-		die();		// Falls keine User_ID angegeben wurde, dann abbrechen
-	
-	$pfad="./spieler/" . intval($_GET['uid']);
-	
-	if(file_exists($pfad . ".jpg"))		// Der Benutzer hat ein JPG-Bild hochgeladen?
-		$suffix="jpg";
-	else
-		if(file_exists($pfad . ".png"))		// oder doch ein PNG?
-			$suffix="png";
-		else
-			if(file_exists($pfad . ".gif"))		// Ne, aber ein GIF
-				$suffix="gif";
-			else {										// Hmm, der User hat gar kein Bild...
-				$bild=ImageCreateFromPNG("./spieler/nopic.png");		// Also das Standardpic einlesen
-				header("content-type: image/png");			// Dem Browser sagen, dass wir ein Bild senden
-				ImagePNG($bild);		// Und das Bild rüber schicken
-				die();		// Zum Schluss abbrechen, denn wir sind ja fertig :)
-			}
-	
-	// Je nachdem, was der Benutzer für einen Typ beim Benutzerbild hat, reagieren wir.
-	// Standardmäßig habe ich mal nur JPG, GIF und PNG erlaubt.
-	
-	// TODO: Transparente PNGs funzen nicht :(
-	switch($suffix) {
-		case "jpg":
-			$bild=ImageCreateFromJPEG($pfad . "." . $suffix);
-			header("content-type: image/jpeg");
-			ImageJPEG($bild, "", 75);
-			break;
-		case "png":
-			$bild=ImageCreateFromPNG($pfad . "." . $suffix);
-			header("content-type: image/png");
-			ImagePNG($bild);
-			break;
-		case "gif":
-			$bild=ImageCreateFromGIF($pfad . "." . $suffix);
-			header("content-type: image/gif");
-			ImageGIF($bild);
-			break;
-	}
-	
-	// Fertisch :)
+<?php
+/**
+ * Liest ein Spielerbild aus und gibt es zurück
+ *
+ * @version 1.0.0
+ * @author Simon Frankenberger <simonfrankenberger@web.de>
+ * @package blm2.includes
+ */
+
+if (!$_GET['uid'])
+    die();        // Falls keine User_ID angegeben wurde, dann abbrechen
+
+$pfad = "./spieler/" . intval($_GET['uid']);
+
+if (file_exists($pfad . ".jpg"))        // Der Benutzer hat ein JPG-Bild hochgeladen?
+    $suffix = "jpg";
+else
+    if (file_exists($pfad . ".png"))        // oder doch ein PNG?
+        $suffix = "png";
+    else
+        if (file_exists($pfad . ".gif"))        // Ne, aber ein GIF
+            $suffix = "gif";
+        else {                                        // Hmm, der User hat gar kein Bild...
+            $bild = ImageCreateFromPNG("./spieler/nopic.png");        // Also das Standardpic einlesen
+            header("content-type: image/png");            // Dem Browser sagen, dass wir ein Bild senden
+            ImagePNG($bild);        // Und das Bild rüber schicken
+            die();        // Zum Schluss abbrechen, denn wir sind ja fertig :)
+        }
+
+// Je nachdem, was der Benutzer für einen Typ beim Benutzerbild hat, reagieren wir.
+// Standardmäßig habe ich mal nur JPG, GIF und PNG erlaubt.
+
+// TODO: Transparente PNGs funzen nicht :(
+switch ($suffix) {
+    case "jpg":
+        $bild = ImageCreateFromJPEG($pfad . "." . $suffix);
+        header("content-type: image/jpeg");
+        ImageJPEG($bild, "", 75);
+        break;
+    case "png":
+        $bild = ImageCreateFromPNG($pfad . "." . $suffix);
+        header("content-type: image/png");
+        ImagePNG($bild);
+        break;
+    case "gif":
+        $bild = ImageCreateFromGIF($pfad . "." . $suffix);
+        header("content-type: image/gif");
+        ImageGIF($bild);
+        break;
+}
+
+// Fertisch :)

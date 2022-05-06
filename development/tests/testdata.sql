6aaa5ad58d5ed61d93621264fd678426094d6b35
Simon Frankenberger <simon@fraho.eu>
2022-05-06 18:00:29 +0200

    remove redundancy from testdata.sql

--- development/tests/testdata.sql
+++ development/tests/testdata.sql
@@ -1,54 +1,25 @@
 alter table mitglieder
     auto_increment 100;
 
-INSERT INTO `mitglieder` (`ID`, `Name`, `EMail`, `Passwort`, `Punkte`)
-VALUES (11, 'test1', 'test1@example.com', sha1('changeit'), 0);
-INSERT INTO `forschung` (`user_id`)
-VALUES (11);
-INSERT INTO `gebaeude` (`user_id`)
-VALUES (11);
-INSERT INTO `lagerhaus` (`user_id`)
-VALUES (11);
-INSERT INTO `punkte` (`user_id`)
-VALUES (11);
-INSERT INTO `statistik` (`user_id`)
-VALUES (11);
+DELIMITER $$
+CREATE PROCEDURE InsertUsers(P_count int)
+BEGIN
+    DECLARE NR int;
+    insertLoop:
+    LOOP
+        SET NR = coalesce(NR, 0) + 1;
 
-INSERT INTO `mitglieder` (`ID`, `Name`, `EMail`, `Passwort`, `Punkte`)
-VALUES (12, 'test2', 'test2@example.com', sha1('changeit'), 0);
-INSERT INTO `forschung` (`user_id`)
-VALUES (12);
-INSERT INTO `gebaeude` (`user_id`)
-VALUES (12);
-INSERT INTO `lagerhaus` (`user_id`)
-VALUES (12);
-INSERT INTO `punkte` (`user_id`)
-VALUES (12);
-INSERT INTO `statistik` (`user_id`)
-VALUES (12);
+        INSERT INTO `mitglieder` (`ID`, `Name`, `EMail`, `Passwort`)
+        VALUES (10 + NR, concat('test', NR), concat('test', NR, '@example.com'), sha1('changeit'));
+        INSERT INTO `forschung` (`user_id`) VALUES (10 + NR);
+        INSERT INTO `gebaeude` (`user_id`) VALUES (10 + NR);
+        INSERT INTO `lagerhaus` (`user_id`) VALUES (10 + NR);
+        INSERT INTO `punkte` (`user_id`) VALUES (10 + NR);
+        INSERT INTO `statistik` (`user_id`) VALUES (10 + NR);
 
-INSERT INTO `mitglieder` (`ID`, `Name`, `EMail`, `Passwort`, `Punkte`)
-VALUES (13, 'test3', 'test3@example.com', sha1('changeit'), 0);
-INSERT INTO `forschung` (`user_id`)
-VALUES (13);
-INSERT INTO `gebaeude` (`user_id`)
-VALUES (13);
-INSERT INTO `lagerhaus` (`user_id`)
-VALUES (13);
-INSERT INTO `punkte` (`user_id`)
-VALUES (13);
-INSERT INTO `statistik` (`user_id`)
-VALUES (13);
+        IF NR = P_count THEN LEAVE insertLoop; END IF;
+    END LOOP insertLoop;
+END $$
+DELIMITER ;
 
-INSERT INTO `mitglieder` (`ID`, `Name`, `EMail`, `Passwort`, `Punkte`)
-VALUES (14, 'test4', 'test4@example.com', sha1('changeit'), 0);
-INSERT INTO `forschung` (`user_id`)
-VALUES (14);
-INSERT INTO `gebaeude` (`user_id`)
-VALUES (14);
-INSERT INTO `lagerhaus` (`user_id`)
-VALUES (14);
-INSERT INTO `punkte` (`user_id`)
-VALUES (14);
-INSERT INTO `statistik` (`user_id`)
-VALUES (14);
+CALL InsertUsers(4);
